3211070eafc49f6b07e9249c86f2e9743bbe6774
dmfriendjr <davidmfriendjr@gmail.com>
2017-12-07 19:51:07 -0500

    Added firebase config and pushing new employee to database

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,29 @@
+  // Initialize Firebase
+var config = {
+	apiKey: "AIzaSyD3vwqgL2IIh9dW3JuHkXCQRPHLPxqp8fI",
+	authDomain: "timesheet-fi.firebaseapp.com",
+	databaseURL: "https://timesheet-fi.firebaseio.com",
+	projectId: "timesheet-fi",
+	storageBucket: "",
+	messagingSenderId: "226258508438"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $('#employeeAddButton').on('click', (event) => {
+	event.preventDefault(); 
+
+	console.log('testing');
 
 	let newEmployee = {
 		email:  $('#emailInput').val(),
 		role: $('#roleInput').val(),
-		startDate: $('#dateInput'),
-		rate: $('#rateInput')
+		startDate: $('#dateInput').val(),
+		rate: $('#rateInput').val()
 	}
+
+	database.ref().push(newEmployee);
 
 	console.log(newEmployee)
 });
